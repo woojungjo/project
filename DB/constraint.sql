f79b8bc1d4ed66d549feb9649e5e1947bc98cda4
Juyeonjoo <yjjjoo0331@gmail.com>
2023-02-17 12:15:47 +0900

    tmp

--- DB/constraint.sql
+++ DB/constraint.sql
@@ -42,3 +42,15 @@ ALTER TABLE mate_board_bookmark ADD CONSTRAINT mate_board_bookmark_post_no_fk FO
 ALTER TABLE mate_board_bookmark ADD CONSTRAINT mate_board_bookmark_member_id_fk FOREIGN KEY (member_id) REFERENCES member(member_id) ON DELETE CASCADE;
 
 ALTER TABLE mate_board_tags ADD CONSTRAINT mate_board_tags_post_no_fk FOREIGN KEY (post_no)REFERENCES mate_board (post_no) ON DELETE CASCADE;
+
+ALTER TABLE member ADD CONSTRAINT member_member_level_num_fk FOREIGN KEY (member_level_num) REFERENCES member_act_grade(member_level_num) ON DELETE CASCADE;
+
+ALTER TABLE member ADD CONSTRAINT member_town_id_fk FOREIGN KEY (town_id) REFERENCES town(town_id) ON DELETE CASCADE;
+
+ALTER TABLE non_member ADD CONSTRAINT non_member_town_id_fk FOREIGN KEY (town_id) REFERENCES town(town_id) ON DELETE CASCADE;
+
+ALTER TABLE member_retail_cart ADD CONSTRAINT member_retail_cart_member_id_fk FOREIGN KEY(member_id) REFERENCES member(member_id) ON DELETE CASCADE;
+
+ALTER TABLE member_retail_cart ADD CONSTRAINT member_retail_cart_retail_id_fk FOREIGN KEY (retail_id) REFERENCES retail(retail_id) ON DELETE CASCADE;
+
+ALTER TABLE  member_retail_cart ADD CONSTRAINT member_retail_cart_cart_id_fk FOREIGN KEY (cart_id ) REFERENCES cart(cart_id) ON DELETE CASCADE;
