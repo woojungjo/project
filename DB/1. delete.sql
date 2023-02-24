1fcca9aa828a4e8b7745d9800d7ba2af93fac0b6
comdesk <deskcom317@gmail.com>
2023-02-24 23:08:47 +0900

    DB 파일 최종

---
+++ DB/1. delete.sql
@@ -0,0 +1,81 @@
+DROP TABLE qna_board_act_not;
+DROP TABLE qna_board_bookmark ;
+DROP TABLE qna_board_keyword_not ;
+DROP TABLE qna_board_act_not ;
+DROP TABLE qna_board_file ;
+DROP TABLE qna_board ;
+DROP TABLE qna_board_comment ;
+DROP TABLE mate_board_tags ;
+DROP TABLE mate_board_keyword_not ;
+DROP TABLE mate_board_bookmark ;
+DROP TABLE mate_board_file ;
+DROP TABLE mate_board_act_not;
+DROP TABLE mate_board_comment;
+DROP TABLE mate_board;
+
+
+DROP TABLE CRAWLING_BOARD;
+DROP TABLE USED_BOARD;
+DROP TABLE CRAWLING_BOARD_BOOKMARK;
+DROP TABLE USED_BOARD_BOOKMARK;
+DROP TABLE CRAWLING_BOARD_ACT_NOT;
+DROP TABLE USED_BOARD_ACT_NOT;
+DROP TABLE CRAWLING_BOARD_KEYWORD_NOT;
+DROP TABLE USED_BOARD_KEYWORD_NOT;
+DROP TABLE CRAWLING_BOARD_COMMENT;
+DROP TABLE USED_BOARD_COMMENT;
+DROP TABLE CRAWLING_BOARD_FILE;
+DROP TABLE USED_BOARD_FILE;
+
+DROP TABLE goods;
+DROP table region_board;
+DROP TABLE sale_board;
+DROP TABLE region_board_comment;
+DROP TABLE sale_board_comment;
+DROP TABLE region_board_file;
+DROP TABLE sale_board_file;
+DROP TABLE member_keyword;
+DROP TABLE region_board_keyword_not;
+DROP TABLE region_board_act_not;
+DROP TABLE region_board_bookmark;
+DROP TABLE sale_board_bookmark;
+DROP TABLE sale_board_act_not;
+DROP TABLE sale_board_keyword_not;
+
+
+drop table withdrawal;
+drop table country;
+drop table city;
+drop table town;
+drop table favorite;
+drop table sale;
+drop table wish_list;
+drop table chat_room_enter;
+
+DROP TABLE MEMBER CASCADE CONSTRAINTS;
+DROP TABLE NON_MEMBER CASCADE CONSTRAINTS;
+DROP TABLE RETAIL CASCADE CONSTRAINTS;
+DROP TABLE CART CASCADE CONSTRAINTS;
+DROP TABLE MEMBER_RETAIL_CART CASCADE CONSTRAINTS;
+
+Drop table goods;
+Drop table member_goods_cart;
+Drop table non_member_retail_cart;
+Drop table non_member_goods_cart;
+Drop table member_act_grade;
+Drop table category;
+Drop table chatroom;
+Drop table standard_value;
+
+DROP TABLE CRAWLING_BOARD;
+DROP TABLE USED_BOARD;
+DROP TABLE CRAWLING_BOARD_BOOKMARK;
+DROP TABLE USED_BOARD_BOOKMARK;
+DROP TABLE CRAWLING_BOARD_ACT_NOT;
+DROP TABLE USED_BOARD_ACT_NOT;
+DROP TABLE CRAWLING_BOARD_KEYWORD_NOT;
+DROP TABLE USED_BOARD_KEYWORD_NOT;
+DROP TABLE CRAWLING_BOARD_COMMENT;
+DROP TABLE USED_BOARD_COMMENT;
+DROP TABLE CRAWLING_BOARD_FILE;
+DROP TABLE USED_BOARD_FILE;
