612981d448a8db45a35a605a0d7319e704787864
jhwan <jhwan.kim95@gmail.com>
2023-04-09 02:40:03 +0900

    change mypage package to map package

--- wecart/src/main/java/org/zerock/wecart/controller/Map.java
+++ wecart/src/main/java/org/zerock/wecart/controller/Map.java
@@ -11,9 +11,9 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 
 
-@RequestMapping("/mypage")
+@RequestMapping("/map")
 @Controller
-public class Mypage {
+public class Map {
 	
 	// 내 주변의 마트를 보여주는 역할을 함
 	@PostMapping("/locationOfMarts")
--- wecart/src/main/webapp/WEB-INF/views/mypage/cart/list.jsp
+++ wecart/src/main/webapp/WEB-INF/views/mypage/cart/list.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+ <h1><%= request.getRequestURI() %></h1>
 </body>
 </html>
