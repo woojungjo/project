96bb0605c4a39c10d314c5c08400ab9a9652d83c
Juyeonjoo <yjjjoo0331@gmail.com>
2023-04-13 07:32:03 +0900

    MateControllerTest error resolved 2023/04/13 07:31 AM

--- wecart/src/main/java/org/zerock/wecart/mapper/board/mateboard/MateBoardMapper.java
+++ wecart/src/main/java/org/zerock/wecart/mapper/board/mateboard/MateBoardMapper.java
@@ -1,5 +1,13 @@
 package org.zerock.wecart.mapper.board.mateboard;
 
-public class MateBoardMapper {
+import java.util.List;
 
-}
+import org.zerock.wecart.domain.mateboard.MateBoardVO;
+
+public interface MateBoardMapper {
+		
+	// 전체 목록 조회, 페이징 처리 전 
+	public abstract List<MateBoardVO> selectAll();
+	
+	
+}//MateBoardMapper
--- wecart/src/main/java/org/zerock/wecart/service/board/mateboard/MateBoardServiceImpl.java
+++ wecart/src/main/java/org/zerock/wecart/service/board/mateboard/MateBoardServiceImpl.java
@@ -1,5 +1,52 @@
 package org.zerock.wecart.service.board.mateboard;
 
-public class MateBoardServiceImpl {
+import java.util.List;
+import java.util.Objects;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.zerock.wecart.domain.mateboard.MateBoardVO;
+import org.zerock.wecart.exception.ServiceException;
+import org.zerock.wecart.mapper.board.mateboard.MateBoardMapper;
+
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.log4j.Log4j2;
+
+@ToString
+@Log4j2
+@NoArgsConstructor
+
+@Service("mateBoardService")
+public class MateBoardServiceImpl implements MateBoardService, InitializingBean{
+	
+	@Setter(onMethod_= {@Autowired})
+	private MateBoardMapper mapper;
+	
+	//빈, 주입잘 받았는지 체크 필요. 
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		log.trace("afterPropertiesSet() invoked");
+		
+		try {
+			Objects.requireNonNull(this.mapper);//ServiceException으로 매핑해서 던지도록 
+		}catch(Exception e) {
+			throw new ServiceException(e);
+		}//try-catch
+	}//afterPropertiesSet() 
+
+	@Override
+	public List<MateBoardVO> getList() throws ServiceException {
+		log.trace("getList()invoked");
+		
+		try {
+			return this.mapper.selectAll();
+		}catch(Exception e) {
+			throw new ServiceException(e);
+		}
+	}//POJO
+	
+ 
 }
