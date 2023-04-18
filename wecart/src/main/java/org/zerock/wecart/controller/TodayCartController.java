5b2b7134488741c4deaa3395e4b677309149cb5f
jhwan <jhwan.kim95@gmail.com>
2023-04-18 09:06:45 +0900

    230418 09:06AM

--- wecart/src/main/java/org/zerock/wecart/controller/TodayCartController.java
+++ wecart/src/main/java/org/zerock/wecart/controller/TodayCartController.java
@@ -1,31 +1,50 @@
 package org.zerock.wecart.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.zerock.wecart.domain.UserVO;
+import org.zerock.wecart.domain.pricecompare.GoodsVO;
+import org.zerock.wecart.exception.ServiceException;
+import org.zerock.wecart.service.pricecompare.PriceCompareService;
 
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 
 
 @NoArgsConstructor
 @Log4j2
 
+@SessionAttributes("__AUTH__")
 @RequestMapping("/todayCart")
 @Controller
 public class TodayCartController {
 
+	@Setter(onMethod_=@Autowired)
+	PriceCompareService service;
+	
 	@GetMapping("/cartList")
 	public void cartList() {
 		log.trace("cartList() invoked.");
 	} //get
 	
 	@PostMapping("/register")
-	public String register() {
-		log.trace("register() invoked.");
+	public GoodsVO register(Model model) throws ServiceException{
+		log.trace("register({}) invoked.", model);
 		
-		return "redirect:cartList";
+		Integer goods_id = (Integer)model.getAttribute("goods_id");
+		GoodsVO goodsVO = service.select(goods_id);
+		
+		
+		UserVO userVO = (UserVO)model.getAttribute("__AUTH__");
+		log.trace("UserVO: {}", userVO);
+		
+		return goodsVO;
 	} //register
 	
 //	@PostMapping("/remove")		Ajax일 때 PostMapping으로
