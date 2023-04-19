031751d65f3948b7d8493c7d6ecf231682b39cb8
user1 <ui007iu@naver.com>
2023-04-19 15:15:14 +0900

    2023/04/19 03:14 PM 아이디찾기 수정, 비밀번호 찾기 진행중,

---
+++ wecart/src/main/java/org/zerock/wecart/domain/user/UserDTO.java
@@ -0,0 +1,12 @@
+package org.zerock.wecart.domain.user;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+	private String login_id;
+	private String pwd;
+	private String alias;
+	private String email;
+
+}
