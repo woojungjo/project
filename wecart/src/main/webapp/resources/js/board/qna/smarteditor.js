ebb723a0d7964035e90f464400c1214652d05d83
Juyeonjoo <yjjjoo0331@gmail.com>
2023-04-29 02:37:13 +0900

    2023/04/29 2:37AM smarteditor view 에서 controller 로 request 보낸 것 받음

--- wecart/src/main/webapp/resources/js/board/qna/smarteditor.js
+++ wecart/src/main/webapp/resources/js/board/qna/smarteditor.js
@@ -10,12 +10,15 @@ var oEditors = [];
         
  */
  var oEditors = [];
+ 
 $(function(){
+	 console.log("NAVER SmartEditor")
       nhn.husky.EZCreator.createInIFrame({
           oAppRef: oEditors,
           elPlaceHolder: "ir1", //textarea에서 지정한 id와 일치해야 합니다. 
           //SmartEditor2Skin.html 파일이 존재하는 경로
           sSkinURI: "/resources/smarteditor/SmartEditor2Skin.html",  
+          fCreator: "createSEditor2",
           htParams : {
               // 툴바 사용 여부 (true:사용/ false:사용하지 않음)
               bUseToolbar : true,             
@@ -24,20 +27,52 @@ $(function(){
               // 모드 탭(Editor | HTML | TEXT) 사용 여부 (true:사용/ false:사용하지 않음)
               bUseModeChanger : true,         
               fOnBeforeUnload : function(){
-                   
               }
           }, 
+          /*
           fOnAppLoad : function(){
               //기존 저장된 내용의 text 내용을 에디터상에 뿌려주고자 할때 사용
               oEditors.getById["ir1"].exec("PASTE_HTML", ["기존 DB에 저장된 내용을 에디터에 적용할 문구"]);
           },
-          fCreator: "createSEditor2"
+          */
       });
       
-      //저장버튼 클릭시 form 전송
-      $("#submitBtn").click(function(){
+      //버튼 클릭시 form 전송
+     $("#registerBtn").click(function(){
           oEditors.getById["ir1"].exec("UPDATE_CONTENTS_FIELD", []);
-          $("#frm").submit();
+          let content = document.getElementById("ir1").value
+          
+		    if(content == '<p><br></p>') { //비어있어도 기본 P태그가 붙더라.
+		        alert("내용을 입력해주세요.")
+		        oEditors.getById["ir1"].exec("FOCUS")
+		        return
+		    } else {
+		        console.log(content)
+		      	let writePost = {
+	            title: $("#smartEditorTitle").val()
+	            ,content: content
+       			 }
+       			let test = JSON.stringify(writePost);
+       			console.log(writePost)
+       			        //ajax 통신으로 서버로 보내 데이터 저장함
+		        $.ajax({
+		            url: "register"
+		            , data: test
+		            , type: 'POST'
+		            ,contentType: 'application/json', 
+		            success: function(data) {
+		                console.log('success')
+		                alert('저장되었습니다.')
+		                //location.href='board/register'
+		            }
+		            , error: function(jqXHR, textStatus, errorThrown) {
+		                console.log(jqXHR)
+		                alert('오류가 발생하였습니다.')
+		            }
+		        })
+		    }
+    
+          $("#register").submit();
       });    
 });
  
