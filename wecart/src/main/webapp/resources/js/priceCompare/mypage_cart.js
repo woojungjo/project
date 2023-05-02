dd1193030786a5a7e105345c1ffc6621853598c0
comdesk <deskcom317@gmail.com>
2023-05-02 15:16:44 +0900

    23/05/02 jy

--- wecart/src/main/webapp/resources/js/priceCompare/mypage_cart.js
+++ wecart/src/main/webapp/resources/js/priceCompare/mypage_cart.js
@@ -1,5 +1,7 @@
 window.onload = function () {
     let cart_id = $('input[name=cart_id]').val();
+    let member_id = $('input[name=member_id]').val();
+    let api_date = new Date($('#cartDate').val());
 
     //합계 구하기
     let count = (function () {
@@ -70,10 +72,29 @@ window.onload = function () {
             });
         } //removeCart
 
+        function currentCompare(cart_id, member_id, api_date, callback, error) {
+            $.ajax({
+                type : 'get',
+                url : '/mypageCart/getCurrent/member/' + member_id + '/cart/' + cart_id + '/date/' + api_date,
+                success : function (currentPrices) {
+                    if(callback) {
+                        console.log(currentPrices);
+                        callback(currentPrices);
+                    }
+                },
+                error : function (xhr, status, er) {
+                    if(error) {
+                        error(er);
+                    }
+                }
+            });
+        } //currentCompare
+
         return {
             totalPrice : totalPrice,
             minusImage : minusImage,
-            removeCart : removeCart
+            removeCart : removeCart,
+            currentCompare : currentCompare
         }
     })();
 
@@ -154,34 +175,34 @@ window.onload = function () {
         bt.addEventListener('click', minus);
     });
 
-    //상품 개별 삭제(자바스크립트 상 삭제)
-    var deleteBts = document.querySelectorAll(".delete_bt");
-    var deleteOne = function (e) {      
-        if(confirm('삭제하시겠습니까?')) {
-            var tr = e.target.closest(".goods_price_tr");
-            tr.remove();
-        } //if 
+    // //상품 개별 삭제(자바스크립트 상 삭제)
+    // var deleteBts = document.querySelectorAll(".delete_bt");
+    // var deleteOne = function (e) {      
+    //     if(confirm('삭제하시겠습니까?')) {
+    //         var tr = e.target.closest(".goods_price_tr");
+    //         tr.remove();
+    //     } //if 
 
-        const totals = document.querySelectorAll(".total_price");
-        totals.forEach((total) => {
-            var tPrice = 0;
-            var index = Array.from(totals).indexOf(total);
+    //     const totals = document.querySelectorAll(".total_price");
+    //     totals.forEach((total) => {
+    //         var tPrice = 0;
+    //         var index = Array.from(totals).indexOf(total);
 
-            const priceTRs = document.querySelectorAll(".goods_price_tr");
-            console.log("index:", index);
+    //         const priceTRs = document.querySelectorAll(".goods_price_tr");
+    //         console.log("index:", index);
 
-            priceTRs.forEach((tr) => {
-                const priceSpans = tr.querySelectorAll(".goods_price");
-                tPrice += parseInt(priceSpans[index].textContent.replace(",", ""));
-            });
+    //         priceTRs.forEach((tr) => {
+    //             const priceSpans = tr.querySelectorAll(".goods_price");
+    //             tPrice += parseInt(priceSpans[index].textContent.replace(",", ""));
+    //         });
             
-            total.textContent = tPrice.toLocaleString();
-        });
-    };
+    //         total.textContent = tPrice.toLocaleString();
+    //     });
+    // };
 
-    deleteBts.forEach((bt) => {
-        bt.addEventListener('click', deleteOne)
-    });
+    // deleteBts.forEach((bt) => {
+    //     bt.addEventListener('click', deleteOne)
+    // });
 
 	//장바구니 삭제 버튼 (DB 삭제)
 	const cartRemoveBt = document.querySelector("#cart_remove_bt");
@@ -191,7 +212,7 @@ window.onload = function () {
                 alert(removeResult);
 
                 window.location = "/mypage/cart/list";
-            })
+            });
         } //if              
     });
 
@@ -199,6 +220,35 @@ window.onload = function () {
     const cartListBt = document.querySelector("#cart_list_bt");
     cartListBt.addEventListener("click", function () {
        location = "/mypage/cart/list"; 
+    });
+
+    let compare = (function () {
+        function currentList(currentPrices) {
+            let str = "";
+            let curDiv = document.querySelectorAll('.price_compare');
+
+            for (let i = 0; i < currentPrices.length; i++) {
+                for (let j = 0; j < currentPrices[i].length; j++) {
+                  console.log(currentPrices[i][j]);
+                  str += "<div class='curPrice'>";
+                  str += "  <div class='curPrice_text'>현재가격</div>"
+                  str += "  <div><span>"+currentPrices[i][j]+"</span>"
+                  str += "       <span>원</span></div>"
+                } //inner for
+            } //outer for
+        } //currentPrices
+
+        return {currentList : currentList}
+
+    })();
+
+    //현재 가격 비교 버튼
+    const currentPriceBt = document.querySelector("#current_price_bt");
+    currentPriceBt.addEventListener('click', function () {
+        count.currentCompare(cart_id, member_id, api_date, function (currentPrices) {
+            console.log('currentPrices:', currentPrices);
+            compare.currentList(currentPrices);
+        });
     });
 };  //.onload
 
