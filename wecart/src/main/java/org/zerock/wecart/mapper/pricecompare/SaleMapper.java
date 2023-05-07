c932806e6a4a24904a4341b79463847db2826e20
JeongHwan95 <jhwan.kim95@gmail.com>
2023-05-07 15:23:43 +0900

    230507 03:22PM
    showPrd/{goods_id} 부분 기능 구현 update

--- wecart/src/main/java/org/zerock/wecart/mapper/pricecompare/SaleMapper.java
+++ wecart/src/main/java/org/zerock/wecart/mapper/pricecompare/SaleMapper.java
@@ -3,6 +3,7 @@ package org.zerock.wecart.mapper.pricecompare;
 import java.sql.Date;
 
 import org.apache.ibatis.annotations.Select;
+import org.zerock.wecart.domain.pricecompare.SaleVO;
 
 public interface SaleMapper {
 	
@@ -11,4 +12,13 @@ public interface SaleMapper {
 			FROM sale
 			""")
 	public abstract Date selectAPIDate();
+	
+	@Select("""
+			SELECT *
+			FROM sale
+			WHERE api_date = #{api_date}
+			AND	retail_id = #{retail_id}
+			AND goods_id = #{goods_id}
+			""" )
+	public abstract SaleVO selectPriceWithRetail_idAndGoods_id(Date api_date, Integer retail_id, Integer goods_id);
 } // end class
--- wecart/src/main/java/org/zerock/wecart/service/pricecompare/PriceCompareService.java
+++ wecart/src/main/java/org/zerock/wecart/service/pricecompare/PriceCompareService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.zerock.wecart.domain.pricecompare.GoodsCriteria;
 import org.zerock.wecart.domain.pricecompare.GoodsVO;
+import org.zerock.wecart.domain.pricecompare.GooodsVO;
+import org.zerock.wecart.domain.pricecompare.PriceDTO;
 import org.zerock.wecart.exception.ServiceException;
 
 public interface PriceCompareService {
@@ -25,4 +27,10 @@ public interface PriceCompareService {
 	// goods_id를 받고 해당 조회수를 ++한다. 만약 update가 안됐으면 false를 return한다.
 	public abstract boolean updateReadcntOfGoods(Integer goods_id) throws ServiceException;
 	
+	// member 아이디에 town_id 와 member가 클릭한 goods을 가지고 
+	// 해당 상품을 판매하는 retail등의 정보를 가지고 있는 PriceDTO 반환  
+	public List<PriceDTO> getPriceDTOList(Integer member_id, Integer goods_id) throws ServiceException;
+	
+	// goods_id를 가지고 GooodsVO 객체를 반환
+	public abstract GooodsVO selectGooodsVO(Integer goods_id) throws ServiceException;
 } //end interface
